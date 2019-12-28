6f5016dc5a95d5c423083807bf206166cd0b8bb2
DanielTisue <d.tisue@gmail.com>
2019-12-28 11:58:10 -0600

    add data file to store project case study writing which will be implented in section components for each individual project.

---
+++ src/project-data-index.js
@@ -0,0 +1,32 @@
+const photo = {
+  title: "Olivieri Photo",
+  summary:
+    "Federico Oliviero is an experienced photographer located in Toronto, Canada. He specializes in Street/City, Event, and Portrait Photography. Federico expressed a need for a website that would showcase his talent. This was a redisgn of a previous website I had built for him. ",
+  context: {
+    role: "UX/UI Designer, Frontend Developer, ",
+    technology:
+      "Adobe: XD, Illustrator, Photoshop. HTML, CSS, Javascript. Node.js, Express, Cloudinary API."
+  },
+  challenge:
+    "Federico wanted a website that would more accurately represent him artistically and woulden't stray from original color coordination of his previous site. He also wanted clients to be able to contact him directly through his website. His ultimate focus was to have a more modern looking site with slightly different functionality.",
+  process: "Initial site UI was done in Abobe XD to reform page layout, page architecture, and design form layout, . Photoshop/ Illustrator were used to design a logo and icons specific to OlivieriPhoto. Node mailer and a contact form was added to the the backend for site messaging capability.",
+  solution: "Federico's site was condensed from four to two pages which allowed the user to have easier navigation. The landing page served most immediate information for quick searchers and minimized clicking effort. Few photos were showcased immediately on the landing page with option to explore his gallery further. Since the re-design Federico has been offered to exhibit his work at a local art gallery in Toronto and has given him more exposure.",
+};
+
+const passion = {
+  title: "Passionate SK8 VIE",
+  summary:
+    "Markos Swcharz is an experienced skateboarder and videographer located in Vienna, Austria. He owns a european based skateboarding company. Markos first goal was to pursue ecommerce. However, the goal wasn't to just sell skateboards but also re-create a skateboard community he grew up with and replicate it on line.The site was to act as an ecommerce, interactive, and videogrpher website.",
+  context: {
+    role: "FullStack Developer, ",
+    technology:
+      "Adobe: XD, Illustrator, Photoshop. MongoDB, React, Node.js, Express, Cloudinary API."
+  },
+  challenge: "Design and build a website that allowed Markos to sell products but also create an online skate community. The site would contain ecommerce functionality, skaters to upload favorite skate spots and comment, and give Markos the ability showcase videos of their favorite local skaters. The techincal aspects would have to further include geolocation, pay option API, Cloudinary API (for both skate spot images & videos).  ",
+  process:
+    "Sketches were made to lay the skeletal groundwork for the site then built upon in Abobe XD: this mapped out the GUI of the website and its navigational architecture. Using React, the eccomerce portion of the page was created and the general landing page. The React ecommerce section existed as a front end application which could then be connected to the Skate spot search web application.",
+  solution: {
+    first: "Coming soon...",
+    second: "The most difficult part of this project was to design and combine an ecommerce site that would communicate with an already existing Node.js/Express web application called the SkateSpots (this was a personal project I built much ealier). "
+  }
+};
