7bd90e12417be13afac36d4785936e7418e5f9de
DanielTisue <d.tisue@gmail.com>
2020-01-01 11:12:26 -0600

    Added two more projects to the case study text. Edited and updated Passionate SK8 VIE challenge, problems, process, and solutions to case study.

--- src/project-data-index.js
+++ src/project-data-index.js
@@ -1,3 +1,6 @@
+//----------------------------------------------
+//Olivieri Photo
+//----------------------------------------------
 const photo = {
   title: "Olivieri Photo",
   summary:
@@ -9,24 +12,83 @@ const photo = {
   },
   challenge:
     "Federico wanted a website that would more accurately represent him artistically and woulden't stray from original color coordination of his previous site. He also wanted clients to be able to contact him directly through his website. His ultimate focus was to have a more modern looking site with slightly different functionality.",
+
   process: "Initial site UI was done in Abobe XD to reform page layout, page architecture, and design form layout, . Photoshop/ Illustrator were used to design a logo and icons specific to OlivieriPhoto. Node mailer and a contact form was added to the the backend for site messaging capability.",
-  solution: "Federico's site was condensed from four to two pages which allowed the user to have easier navigation. The landing page served most immediate information for quick searchers and minimized clicking effort. Few photos were showcased immediately on the landing page with option to explore his gallery further. Since the re-design Federico has been offered to exhibit his work at a local art gallery in Toronto and has given him more exposure.",
+
+  solution: "Federico's site was condensed from four to two pages which allowed the user to have easier navigation. The landing page served most immediate information for quick searchers and minimized clicking effort. Photos were showcased immediately on the landing page with option to explore his gallery further. Since the re-design Federico has been offered to exhibit his work at a local art gallery in Toronto, and the website has given him more exposure.",
 };
+
+//----------------------------------------------
+//Passion SkateBoarding Ecomm
+//----------------------------------------------
 
 const passion = {
   title: "Passionate SK8 VIE",
   summary:
-    "Markos Swcharz is an experienced skateboarder and videographer located in Vienna, Austria. He owns a european based skateboarding company. Markos first goal was to pursue ecommerce. However, the goal wasn't to just sell skateboards but also re-create a skateboard community he grew up with and replicate it on line.The site was to act as an ecommerce, interactive, and videogrpher website.",
+    "Markos Swcharz is an experienced skateboarder and videographer located in Vienna, Austria. He owns a european based skateboarding company. Markos's first goal was to pursue ecommerce. However, the goal wasn't to just sell skateboards but also re-create a skateboard community he grew up with and replicate it on line. The site was to act as an ecommerce, interactive, and showcase his videographer skills.",
   context: {
     role: "FullStack Developer, ",
     technology:
-      "Adobe: XD, Illustrator, Photoshop. MongoDB, React, Node.js, Express, Cloudinary API."
+      "Adobe: XD, Illustrator, Photoshop. React, Node.js, Express, Cloudinary API."
   },
-  challenge: "Design and build a website that allowed Markos to sell products but also create an online skate community. The site would contain ecommerce functionality, skaters to upload favorite skate spots and comment, and give Markos the ability showcase videos of their favorite local skaters. The techincal aspects would have to further include geolocation, pay option API, Cloudinary API (for both skate spot images & videos).  ",
+  challenge: "Design and build a website that allowed Markos to sell products but also create an online skate community. The site would contain ecommerce functionality, skaters to upcoming events such as giveways, contests, etc. The techincal aspects would have to further include pay option API, Cloudinary API (for videos), and way to display the latest events.  ",
   process:
-    "Sketches were made to lay the skeletal groundwork for the site then built upon in Abobe XD: this mapped out the GUI of the website and its navigational architecture. Using React, the eccomerce portion of the page was created and the general landing page. The React ecommerce section existed as a front end application which could then be connected to the Skate spot search web application.",
+    "Sketches were made to lay the skeletal groundwork for the site then built upon in Abobe XD: this mapped out the GUI of the website (such as icons, colors, typography) and its navigational architecture. Using React, the eccomerce portion of the page was created and the general landing page. The React ecommerce section existed as a front end application which would also serve as a Progressive Web App for mobile users.",
+  problems: {
+    second: "The most difficult part of this project was to design and combine an ecommerce site that would primarily exist as a frontend aplication.",
+    solution: "Use api calls to display upcoming events which allowed the application to still exist as more of a frontend for eccommerce. Site speed was primary focus to ensure credibility."
+  },
+  solution: "Markos has begun marketing his site and stated he is shock and amazed at the site. This was a good thing. "
+  
+};
+
+//----------------------------------------------
+//Skate Spots
+//----------------------------------------------
+
+const skate = {
+  title: "Skate Spots",
+  summary:
+    "This was a personal project that was created to help skateboarders find skatespots across the globe. Originally skateboarders would first have to find a local skateshop and get to know poeple in order to get the latest updates on new skate spots. Even now, only the main stream skate parks are located on google and do not include specific street spots such as curbs, stairs, ledges, and handrails.",
+  context: {
+    role: "FullStack Developer, ",
+    technology:
+      "Adobe: XD, Illustrator, Photoshop. MongoDB, EJS, NodeJs, Express, Cloudinary API."
+  },
+  challenge: " The site had to include the users knowledge as the data entry for a variety of spots. Initially, deployed with little design UI in mind, it needed a facelift. More specifically it needed a brand.",
+  process: {
+   code: "Mongo NoSQL database was used for user authentication schemas, and skatespot schemas. Cloudinary was added so that the user could upload their own images to display spots. Geolocation gave the user to ability to find each spot more specifically.",
+   design: "Used adobe XD to re-design a landing page. In this design more features needed to be added to the landing page. The branding was re-developed using guidelines that would invovle a set of rules for buttons, icons, and colors. "
+  },
+
+  problems: {
+    problem: "The most difficult parts of this project were implementing user authenication without limiting the user to viewing spots and implenting cloudinary api & geolocation with async functions. Testing extensively to find the right programmatic and functional order.",
+
+    solution: "Users now have an updated design focused site that serves as a global skate spot finder as well as a skateboarder community. "
+
+  }
+
+};
+
+//----------------------------------------------
+//Drinkify Cocktail Search
+//----------------------------------------------
+
+const drink = {
+  title: "Drink'd",
+  summary:
+    "This was a personal project that allows the User to search different cocktail recipes. ",
+  context: {
+    role: "Frontend Developer",
+    technology:
+      "Adobe: XD, Illustrator, Photoshop. React, CocktailDB API."
+  },
+  challenge: "Design and build a Progessive Web App.The biiggest challenge to this project was finding a way to parse the ingredient data which wasn't represented in your usual array form. Each ingredient was listed as its own individual object.",
+  process:
+    "I used Adobe XD to map mobile and desktop UI layout. The application .",
   solution: {
     first: "Coming soon...",
     second: "The most difficult part of this project was to design and combine an ecommerce site that would communicate with an already existing Node.js/Express web application called the SkateSpots (this was a personal project I built much ealier). "
   }
-};
+
+}
